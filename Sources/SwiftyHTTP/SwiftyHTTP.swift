08499334dcc5ae48647e055e6a0e2df0c5b8feb4
FanLemon <iyangy@gmail.com>
2021-11-29 15:56:16 +0900

    Async Version 1.0.0

--- Sources/SwiftyHTTP/SwiftyHTTP.swift
+++ Sources/SwiftyHTTP/SwiftyHTTP.swift
@@ -1,6 +1,204 @@
-public struct SwiftyHTTP {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+
+
+protocol HTTPSession {
+    
+    var urlSession: URLSession { get }
+    
+    var baseURLString: String { get }
+}
+
+
+enum HTTPMethod: String {
+    
+    case GET = "GET"
+    
+    case POST = "POST"
+    
+    case PUT = "PUT"
+    
+    case PATCH = "PATCH"
+    
+    case DELETE = "DELETE"
+}
+
+
+enum HTTPSessionParameter {
+    
+    case none
+    
+    case urlEncoding(parameters: [String: Any])
+    
+    case postJSON(parameters: [String: Any])
+        
+    case postXML(parameters: [String: Any])
+}
+
+
+enum HTTPResponseDataType {
+    
+    case text
+    
+    case json
+    
+    case xml
+}
+
+
+enum HTTPSessionError {
+    
+    case baseURLInvalid(baseURL: String)
+    
+    case urlEncodingError
+    
+    case parametersJSONEncodingError(parameters: [String: Any])
+    
+    case requestError(error: Error)
+    
+    case responseFailed
+    
+    case httpStatusCodeInvalid
+    
+    case noResponsedData
+    
+    case decodingJSONDataError(jsonData: Data)
+}
+
+
+enum HTTPSessionResponse<T> {
+    
+    case success(T)
+    
+    case failure(error: HTTPSessionError)
+}
+
+
+typealias HTTPSessionHeaders = [String: String]
+
+
+protocol HTTPURLRoute {
+    
+    var path: String { get }
+    
+    var method: HTTPMethod { get }
+    
+    var headers: HTTPSessionHeaders { get }
+    
+    var parameter: HTTPSessionParameter { get }
+    
+    var responseDataType: HTTPResponseDataType { get }
+    
+    @discardableResult func request<T>(httpSession: HTTPSession, dataType: T.Type, completion: @escaping (_ response: HTTPSessionResponse<T>) -> Void) -> URLSessionTask? where T: Decodable
+}
+
+
+extension URLComponents {
+    
+    init?(routeURL: URL, parameters: [String: Any]) {
+        self.init(url: routeURL, resolvingAgainstBaseURL: false)
+        
+        queryItems = parameters.map({ key, value in
+            return URLQueryItem(name: key, value: String(describing: value))
+        })
+    }
+}
+
+
+extension HTTPURLRoute {
+    
+    @discardableResult func request<T>(httpSession: HTTPSession, dataType: T.Type, completion: @escaping (_ response: HTTPSessionResponse<T>) -> Void) -> URLSessionTask? where T: Decodable {
+        
+        guard let baseURL = URL(string: httpSession.baseURLString) else {
+            completion(HTTPSessionResponse.failure(error: HTTPSessionError.baseURLInvalid(baseURL: httpSession.baseURLString)))
+            return nil
+        }
+        
+        var routeURL = baseURL.appendingPathComponent(path)
+        var bodyData: Data?
+        
+        switch parameter {
+        case .urlEncoding(let paras):
+            guard let encodedURL = URLComponents(routeURL: routeURL, parameters: paras)?.url else {
+                completion(HTTPSessionResponse.failure(error: HTTPSessionError.urlEncodingError))
+                return nil
+            }
+            
+            routeURL = encodedURL
+            
+        case .postJSON(let paras):
+            bodyData = try? JSONSerialization.data(withJSONObject: paras, options: [])
+            
+            if nil == bodyData {
+                completion(HTTPSessionResponse.failure(error: HTTPSessionError.parametersJSONEncodingError(parameters: paras)))
+                return nil
+            }
+            
+        case .postXML(let paras):
+            print("It looks like too old school. Objects: [\(paras)]")
+            break
+            
+        default:
+            break
+        }
+        
+        
+        var urlRequest = URLRequest(url: routeURL)
+        
+        urlRequest.httpMethod = method.rawValue
+        
+        headers.forEach { (key: String, value: String) in
+            urlRequest.addValue(value, forHTTPHeaderField: key)
+        }
+        
+        urlRequest.httpBody = bodyData
+                
+        
+        let task = httpSession.urlSession.dataTask(with: urlRequest) { data, urlResponse, error in
+            
+            if let err = error {
+                print("urlSession.dataTask error: [\(err)]")
+                completion(HTTPSessionResponse.failure(error: HTTPSessionError.requestError(error: err)))
+                return
+            }
+            
+            guard let response = urlResponse as? HTTPURLResponse else {
+                completion(HTTPSessionResponse.failure(error: HTTPSessionError.responseFailed))
+                return
+            }
+            
+            switch response.statusCode {
+            case 200...299:
+                guard let respData = data else {
+                    completion(HTTPSessionResponse.failure(error: HTTPSessionError.noResponsedData))
+                    return
+                }
+                
+                switch self.responseDataType {
+                case .json:
+                    let decoder = JSONDecoder()
+                    guard let model = try? decoder.decode(T.self, from: respData) else {
+                        completion(HTTPSessionResponse.failure(error: HTTPSessionError.decodingJSONDataError(jsonData: respData)))
+                        return
+                    }
+                    
+                    completion(HTTPSessionResponse.success(model))
+                    
+                case .text:
+                    print("Responsed Data is Text.")
+                    
+                default:
+                    print("It looks like too old school. Responsed Data Length: [\(respData.count)]")
+                }
+                
+            default:
+                print("HTTP Responsed Status Code: [\(response.statusCode)]")
+                completion(HTTPSessionResponse.failure(error: HTTPSessionError.httpStatusCodeInvalid))
+                return
+            }
+        }
+                
+        task.resume()
+        return task
     }
 }
--- Tests/SwiftyHTTPTests/SwiftyHTTPTests.swift
+++ Tests/SwiftyHTTPTests/SwiftyHTTPTests.swift
@@ -1,11 +1,233 @@
 import XCTest
 @testable import SwiftyHTTP
 
-final class SwiftyHTTPTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftyHTTP().text, "Hello, World!")
+//
+// https://reqres.in/api/login
+//
+
+
+class APITestHTTPSession: HTTPSession {
+    
+    var urlSession: URLSession = URLSession(configuration: URLSessionConfiguration.default)
+    
+    var baseURLString: String = "https://reqres.in/"
+    
+    init() {
+        
+    }
+    
+    static let shared = APITestHTTPSession()
+}
+
+
+enum APITestUserModule: HTTPURLRoute {
+    
+    case login(email: String, password: String)
+    
+    case singleUser
+    
+    case users(page: Int)
+    
+    var path: String {
+        switch self {
+        case .login( _, _):
+            return "api/login"
+            
+        case .singleUser:
+            return "api/users/2"
+            
+        case .users( _):
+            return "api/users"
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .login( _, _):
+            return .POST
+        case .singleUser:
+            return .GET
+        case .users( _):
+            return .GET
+        }
+    }
+    
+    var headers: [String : String] {
+        return ["Content-Type": "application/json"]
+    }
+     
+    //
+    //    {
+    //        "email": "eve.holt@reqres.in",
+    //        "password": "cityslicka"
+    //    }
+    //
+    var parameter: HTTPSessionParameter {
+        switch self {
+        case .login( let email, let password):
+            return HTTPSessionParameter.postJSON(parameters: ["email": email, "password": password])
+        case .singleUser:
+            return HTTPSessionParameter.none
+        case .users( let page):
+            return HTTPSessionParameter.urlEncoding(parameters: ["page" : page])
+        }
+    }
+        
+    var responseDataType: HTTPResponseDataType {
+        return .json
     }
 }
+
+
+//
+// {
+//     "token": "QpwL5tke4Pnpja7X4"
+// }
+//
+struct UserLoginStatus: Codable {
+    
+    var token: String
+}
+
+
+//
+//    {
+//        "data": {
+//            "id": 2,
+//            "email": "janet.weaver@reqres.in",
+//            "first_name": "Janet",
+//            "last_name": "Weaver",
+//            "avatar": "https://reqres.in/img/faces/2-image.jpg"
+//        },
+//        "support": {
+//            "url": "https://reqres.in/#support-heading",
+//            "text": "To keep ReqRes free, contributions towards server costs are appreciated!"
+//        }
+//    }
+//
+struct UserData: Codable {
+    
+    var id: Int
+    
+    var email: String
+    
+    var first_name: String
+    
+    var last_name: String
+    
+    var avatar: String
+}
+
+struct UserSupport: Codable {
+    
+    var url: String
+    
+    var text: String
+}
+
+struct User: Codable {
+    
+    var data: UserData
+    
+    var support: UserSupport
+}
+
+struct UsersPage: Codable {
+    
+    var page: Int
+    
+    var per_page: Int
+    
+    var total: Int
+    
+    var total_pages: Int
+    
+    var data: [UserData]
+    
+    var support: UserSupport
+}
+
+
+
+final class SwiftyHTTPTests: XCTestCase {
+    
+    func testPost() throws {
+        
+        var loginStatus = UserLoginStatus(token: "")
+        let semaphore = DispatchSemaphore(value: 0)
+        
+        APITestUserModule.login(email: "eve.holt@reqres.in", password: "cityslicka").request(httpSession: APITestHTTPSession.shared, dataType: UserLoginStatus.self) { response in
+            
+            switch response {
+            case .success(let status):
+                print("Login return status: [\(status)]")
+                loginStatus = status
+                semaphore.signal()
+                
+            case .failure(let err):
+                print("User Login Error: \(err)")
+                semaphore.signal()
+            }
+        }
+        
+        _ = semaphore.wait(timeout: DispatchTime.now() + 6)
+        
+        XCTAssertEqual(loginStatus.token, "QpwL5tke4Pnpja7X4")
+    }
+    
+    func testGet() throws {
+        
+        var singleUser = User(data: UserData(id: 0, email: "", first_name: "", last_name: "", avatar: ""),
+                              support: UserSupport(url: "", text: ""))
+        
+        let semaphore = DispatchSemaphore(value: 0)
+        
+        APITestUserModule.singleUser.request(httpSession: APITestHTTPSession.shared, dataType: User.self) { response in
+            
+            switch response {
+            case .success(let user):
+                print("User [\(user)]")
+                singleUser = user
+                semaphore.signal()
+                
+            case .failure(let err):
+                print("User get single user Error: \(err)")
+                semaphore.signal()
+            }
+        }
+        
+        _ = semaphore.wait(timeout: DispatchTime.now() + 6)
+        
+        XCTAssertEqual(singleUser.data.first_name, "Janet")
+        XCTAssertEqual(singleUser.data.last_name, "Weaver")
+    }
+    
+    func testURLEncoding() throws {
+        
+        var pageUser = UsersPage(page: 0, per_page: 0, total: 0, total_pages: 0, data: [], support: UserSupport(url: "", text: ""))
+        
+        let semaphore = DispatchSemaphore(value: 0)
+        
+        APITestUserModule.users(page: 2).request(httpSession: APITestHTTPSession.shared, dataType: UsersPage.self) { response in
+            
+            switch response {
+            case .success(let page):
+                print("Page User [\(page)]")
+                pageUser = page
+                semaphore.signal()
+                
+            case .failure(let err):
+                print("User get page user Error: \(err)")
+                semaphore.signal()
+            }
+        }
+        
+        _ = semaphore.wait(timeout: DispatchTime.now() + 6)
+        
+        XCTAssertEqual(pageUser.data.count, 6)
+        XCTAssertEqual(pageUser.support.text, "To keep ReqRes free, contributions towards server costs are appreciated!")
+    }
+    
+}
+
+
